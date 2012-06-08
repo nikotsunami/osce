5a957cce84a2c9d173a937ccd20ebccb36786bda
Daisy Yang <daisy@bizpoint.cn>
2012-06-08 14:57:03 +0800

    Add AnamnesisCheckTitle entity

---
+++ OsceManager/src/test/java/ch/unibas/medizin/osce/domain/AnamnesisCheckTitleIntegrationTest.java
@@ -0,0 +1,12 @@
+package ch.unibas.medizin.osce.domain;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = AnamnesisCheckTitle.class)
+public class AnamnesisCheckTitleIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
