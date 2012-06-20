89b84825111721c935185dac6c7b2db1ef19bd17
Paul Smout <paul@bizpoint.cn>
2012-06-21 00:19:34 +0800

    removed debug breaking build

--- OsceManager/src/main/java/ch/unibas/medizin/osce/server/DMZSyncServiceImpl.java
+++ OsceManager/src/main/java/ch/unibas/medizin/osce/server/DMZSyncServiceImpl.java
@@ -621,9 +621,6 @@ public class DMZSyncServiceImpl extends RemoteServiceServlet implements
 	
 	HttpServletRequest getRequest(){
 		HttpServletRequest ret = getThreadLocalRequest();
-		if (ret == null ){
-			ret = GroovyDMZSyncServiceImpl.request;
-		}
 		return ret;
 	}
 	
