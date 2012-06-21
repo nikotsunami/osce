4ceb8e81209a8e514bc669a21b0931f856ff3445
Paul Smout <paul@bizpoint.cn>
2012-06-21 15:34:44 +0800

    fixed duplicate data after merge

--- OsceManager/src/main/java/import.sql
+++ OsceManager/src/main/java/import.sql
@@ -62,15 +62,7 @@ INSERT INTO `anamnesis_check_title` (id, sort_order, text, version) VALUES (1, 1
 INSERT INTO `anamnesis_check_title` (id, sort_order, text, version) VALUES (2, 3, 'Krankengeschichte', 0);
 INSERT INTO `anamnesis_check_title` (id, sort_order, text, version) VALUES (3, 2, 'Behandlungsgeschichte', 0);
 
-INSERT INTO `anamnesis_check` (id,sort_order,value,type,user_specified_order,text,version,anamnesis_check_title) VALUES (1,2,'',1,NULL,'Rauchen Sie?',0,1);
-INSERT INTO `anamnesis_check` (id,sort_order,value,type,user_specified_order,text,version,anamnesis_check_title) VALUES (2,3,'oft|mittel|selten',2,NULL,'Wie oft rauchen Sie?',0,1);
-INSERT INTO `anamnesis_check` (id,sort_order,value,type,user_specified_order,text,version,anamnesis_check_title) VALUES (9,4,'Marlboro|Mary Long|Lucky Strike|Awesomesauce|Winfail',3,NULL,'Welche Zigarettenmarken haben Sie schon geraucht?',0,1);
-INSERT INTO `anamnesis_check` (id,sort_order,value,type,user_specified_order,text,version,anamnesis_check_title) VALUES (5,6,'',1,NULL,'Leiden Sie unter Diabetes?',0,2);
-INSERT INTO `anamnesis_check` (id,sort_order,value,type,user_specified_order,text,version,anamnesis_check_title) VALUES (6,7,'',1,NULL,'Wurde Ihnen der Blinddarm entfernt?',0,2);
-INSERT INTO `anamnesis_check` (id,sort_order,value,type,user_specified_order,text,version,anamnesis_check_title) VALUES (8,8,'Darmende|Kopf|Fuss|Iris',2,NULL,'Woraus wurde Ihnen der Blinddarm entfernt?',0,2);
-INSERT INTO `anamnesis_check` (id,sort_order,value,type,user_specified_order,text,version,anamnesis_check_title) VALUES (3,10,'',1,NULL,'Nehmen Sie zurzeit regelmässig Medikamente ein?',0,3);
-INSERT INTO `anamnesis_check` (id,sort_order,value,type,user_specified_order,text,version,anamnesis_check_title) VALUES (4,11,'',0,NULL,'Welche Medikamente nehmen Sie ein?',0,3);
-INSERT INTO `anamnesis_check` (id,sort_order,value,type,user_specified_order,text,version,anamnesis_check_title) VALUES (7,12,'Prozac|Ritalin|Aspirin|Ethanol',3,NULL,'Nehmen Sie eines der aufgelisteten Medikamete und wenn ja, welche?',0,3);
+
 INSERT INTO `anamnesis_check` (id,sort_order,value,type,text,version,anamnesis_check_title) VALUES (1,1,'',1,'Rauchen Sie?',0,1);
 INSERT INTO `anamnesis_check` (id,sort_order,value,type,text,version,anamnesis_check_title) VALUES (2,2,'oft|mittel|selten',2,'Wie oft rauchen Sie?',0,1);
 INSERT INTO `anamnesis_check` (id,sort_order,value,type,text,version,anamnesis_check_title) VALUES (9,3,'Marlboro|Mary Long|Lucky Strike|Awesomesauce|Winfail',3,'Welche Zigarettenmarken haben Sie schon geraucht?',0,1);
