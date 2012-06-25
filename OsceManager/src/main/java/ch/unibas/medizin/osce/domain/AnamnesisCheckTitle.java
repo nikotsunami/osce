a164ab34732668f14aad6b7d10087c9f062351ec
Paul Smout <paul@bizpoint.cn>
2012-06-25 18:13:05 +0800

    moved method out of aj file

--- OsceManager/src/main/java/ch/unibas/medizin/osce/domain/AnamnesisCheckTitle.java
+++ OsceManager/src/main/java/ch/unibas/medizin/osce/domain/AnamnesisCheckTitle.java
@@ -24,88 +24,88 @@ public class AnamnesisCheckTitle {
     private String text;
 
     private Integer sort_order;
-    
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((sort_order == null) ? 0 : sort_order.hashCode());
-		result = prime * result + ((text == null) ? 0 : text.hashCode());
-		return result;
-	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		AnamnesisCheckTitle other = (AnamnesisCheckTitle) obj;
-		if (sort_order == null) {
-			if (other.sort_order != null)
-				return false;
-		} else if (!sort_order.equals(other.sort_order))
-			return false;
-		if (text == null) {
-			if (other.text != null)
-				return false;
-		} else if (!text.equals(other.text))
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((sort_order == null) ? 0 : sort_order.hashCode());
+        result = prime * result + ((text == null) ? 0 : text.hashCode());
+        return result;
+    }
 
-    
-    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AnamnesisCheckTitle other = (AnamnesisCheckTitle) obj;
+        if (sort_order == null) {
+            if (other.sort_order != null)
+                return false;
+        } else if (!sort_order.equals(other.sort_order))
+            return false;
+        if (text == null) {
+            if (other.text != null)
+                return false;
+        } else if (!text.equals(other.text))
+            return false;
+        return true;
+    }
+
+
+
     public void insertNewSortOder(Integer previousSortOder){
-    	//TODO 
-    	System.out.println("!!!!!!!!!!!!!!!!this is insertNewSortOder ");
-    	if(previousSortOder == 0){
-    	List<AnamnesisCheckTitle> anamnesisCheckTitles = findAllAnamnesisCheckTitles();
-    	for(AnamnesisCheckTitle anamnesisCheckTitleBlow : anamnesisCheckTitles){
-    		if(anamnesisCheckTitleBlow.sort_order != null){
-    			anamnesisCheckTitleBlow.sort_order = anamnesisCheckTitleBlow.sort_order + 1;
-    			anamnesisCheckTitleBlow.persist();
-    		}
-    	}
-    	this.sort_order = 1;
-    	this.persist();
-    	}
+        //TODO
+        System.out.println("!!!!!!!!!!!!!!!!this is insertNewSortOder ");
+        if(previousSortOder == 0){
+        List<AnamnesisCheckTitle> anamnesisCheckTitles = findAllAnamnesisCheckTitles();
+        for(AnamnesisCheckTitle anamnesisCheckTitleBlow : anamnesisCheckTitles){
+            if(anamnesisCheckTitleBlow.sort_order != null){
+                anamnesisCheckTitleBlow.sort_order = anamnesisCheckTitleBlow.sort_order + 1;
+                anamnesisCheckTitleBlow.persist();
+            }
+        }
+        this.sort_order = 1;
+        this.persist();
+        }
     }
-    
-    
-    
-	private static List<AnamnesisCheckTitle> getReSortingList(Integer sortFrom) {
 
-		System.out.println(">>>>>>>>>sortFrom = "+sortFrom);
-		EntityManager em = AnamnesisCheck.entityManager();
-		TypedQuery<AnamnesisCheckTitle> q = em
-				.createQuery(
-						"SELECT o FROM AnamnesisCheckTitle AS o WHERE o.sort_order >= :sortFrom ORDER BY sort_order ASC",
-						AnamnesisCheckTitle.class);
-		q.setParameter("sortFrom", sortFrom);
-		if (q.getResultList() == null || q.getResultList().size() == 0) {
-			return null;
-		}
 
-		return q.getResultList();
 
-	}
-    
+    private static List<AnamnesisCheckTitle> getReSortingList(Integer sortFrom) {
+
+        System.out.println(">>>>>>>>>sortFrom = "+sortFrom);
+        EntityManager em = AnamnesisCheck.entityManager();
+        TypedQuery<AnamnesisCheckTitle> q = em
+                .createQuery(
+                        "SELECT o FROM AnamnesisCheckTitle AS o WHERE o.sort_order >= :sortFrom ORDER BY sort_order ASC",
+                        AnamnesisCheckTitle.class);
+        q.setParameter("sortFrom", sortFrom);
+        if (q.getResultList() == null || q.getResultList().size() == 0) {
+            return null;
+        }
+
+        return q.getResultList();
+
+    }
+
     public static void reSorting(Integer sortFrom){
-    	List<AnamnesisCheckTitle> reSortingList = getReSortingList(sortFrom);
-    	if(reSortingList != null){
-    		for (AnamnesisCheckTitle checkTitle : reSortingList) {
-    			if(checkTitle.sort_order != null){
-    				checkTitle.sort_order = checkTitle.sort_order - 1;
-    				checkTitle.persist();
-    			}
-    			
-    		}
-    	}
+        List<AnamnesisCheckTitle> reSortingList = getReSortingList(sortFrom);
+        if(reSortingList != null){
+            for (AnamnesisCheckTitle checkTitle : reSortingList) {
+                if(checkTitle.sort_order != null){
+                    checkTitle.sort_order = checkTitle.sort_order - 1;
+                    checkTitle.persist();
+                }
+
+            }
+        }
     }
-    
+
     public void moveUp() {
         if (this.entityManager == null) {
             this.entityManager = entityManager();
@@ -117,7 +117,7 @@ public class AnamnesisCheckTitle {
         anamnesisCheckTitle.setSort_order(this.sort_order);
         anamnesisCheckTitle.persist();
         setSort_order(sort_order - 1);
-        this.persist();    	
+        this.persist();
     }
 
     public void moveDown() {
@@ -133,7 +133,7 @@ public class AnamnesisCheckTitle {
         setSort_order(sort_order + 1);
         this.persist();
     }
-    
+
     //find next title
     private AnamnesisCheckTitle findAnamnesisCheckTitleByOrderGreater(int sort_order) {
         EntityManager em = entityManager();
@@ -161,5 +161,8 @@ public class AnamnesisCheckTitle {
             return null;
         return resultList.get(0);
     }
-    
+
+    public static List<AnamnesisCheckTitle> findAllAnamnesisCheckTitles() {
+        return entityManager().createQuery("SELECT o FROM AnamnesisCheckTitle o ORDER BY sort_order", AnamnesisCheckTitle.class).getResultList();
+    }
 }
