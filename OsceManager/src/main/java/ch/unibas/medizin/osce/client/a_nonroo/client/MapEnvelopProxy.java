0e9062ebd88966c12eb9c735247700ff00bd446e
milan <milan@MILAN.SPEC.com>
2012-11-19 17:40:52 +0530

    Module 16:Item Analysis

--- OsceManager/src/main/java/ch/unibas/medizin/osce/client/a_nonroo/client/MapEnvelopProxy.java
+++ OsceManager/src/main/java/ch/unibas/medizin/osce/client/a_nonroo/client/MapEnvelopProxy.java
@@ -3,7 +3,6 @@ package ch.unibas.medizin.osce.client.a_nonroo.client;
 import java.util.List;
 
 import ch.unibas.medizin.osce.shared.MapEnvelop;
-import ch.unibas.medizin.osce.shared.MapEnvelopKeyValuePair;
 
 import com.google.gwt.requestfactory.shared.ProxyFor;
 import com.google.gwt.requestfactory.shared.ValueProxy;
