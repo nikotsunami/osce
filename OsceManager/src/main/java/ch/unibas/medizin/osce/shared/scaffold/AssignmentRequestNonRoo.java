98e29917d404bedbf3ab4fe8864ac7e6343136b7
abhay <abhay.gohel@spec-india.com>
2013-02-22 10:55:34 +0530

    Bug Fix.

--- OsceManager/src/main/java/ch/unibas/medizin/osce/shared/scaffold/AssignmentRequestNonRoo.java
+++ OsceManager/src/main/java/ch/unibas/medizin/osce/shared/scaffold/AssignmentRequestNonRoo.java
@@ -3,7 +3,6 @@ package ch.unibas.medizin.osce.shared.scaffold;
 import java.util.Date;
 import java.util.List;
 
-import ch.unibas.medizin.osce.client.a_nonroo.client.StudentAssignmentValueProxy;
 import ch.unibas.medizin.osce.client.managed.request.AssignmentProxy;
 import ch.unibas.medizin.osce.client.managed.request.DoctorProxy;
 import ch.unibas.medizin.osce.client.managed.request.PatientInRoleProxy;
