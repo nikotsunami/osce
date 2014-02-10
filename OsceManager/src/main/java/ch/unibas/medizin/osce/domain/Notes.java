1d65200903398f7d09aee1acb6a448ca388447d8
Abhay Gohel <abhay.gohel@spec-india.com>
2014-02-10 15:10:34 +0530

    Import notes data from eOSCE files.

---
+++ OsceManager/src/main/java/ch/unibas/medizin/osce/domain/Notes.java
@@ -0,0 +1,38 @@
+package ch.unibas.medizin.osce.domain;
+
+import java.util.Date;
+
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooEntity
+public class Notes {
+	
+	@ManyToOne
+	private Student student;
+	
+	@ManyToOne
+	private Doctor doctor;
+
+	@ManyToOne
+	private OscePostRoom oscePostRoom;
+	
+	@ManyToOne
+	private OsceDay osceDay;
+	
+	@Size(max=5000)
+	private String comment;
+		
+	@Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date lastviewed;
+}
