99d5adc59bd9ba21094f43b42a8f62090618712c
poojat <pooja.thesia@spec-india.com>
2014-11-07 12:08:09 +0530

    Bug fix.

--- OsceManager/src/main/java/ch/unibas/medizin/osce/server/util/file/XmlUtil.java
+++ OsceManager/src/main/java/ch/unibas/medizin/osce/server/util/file/XmlUtil.java
@@ -119,7 +119,7 @@ public class XmlUtil {
 				Element queArray = doc.createElement("array");
 				topicDict.appendChild(queArray);
 				
-				List<ChecklistItem> checklistQuestionList = ChecklistItem.findChecklistQuestionByChecklistTopicId(checklistTopic.getId());
+				List<ChecklistItem> checklistQuestionList = ChecklistItem.findChecklistQuestionByChecklistId(checklistTopic.getId());
 				//Iterator<ChecklistQuestion> queItr = checklistTopic.getCheckListQuestions().iterator();
 				
 				//while (queItr.hasNext())
